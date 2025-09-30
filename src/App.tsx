c6cd95849afae8d16b4c6ae222ca4fd685bf8c96
Rubo <lucasrubo1@gmail.com>
2025-09-30 00:06:34 -0300

    fix: route

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/print" element={<PrintPortfolio />} />
